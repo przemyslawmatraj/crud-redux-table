7b79437e6d38fa9cce75d14a819396cfb864e2f1
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-03-24 02:15:03 +0100

    chore: downgrade react to 17.0.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider as ReactReduxProvider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <ReactReduxProvider store={store}>
       <App />
     </ReactReduxProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
