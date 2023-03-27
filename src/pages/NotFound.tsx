44ecc974d500bb28e4f74a7f8b173272e95695fe
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-03-28 00:21:31 +0200

    refactor: not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,31 @@
+import { Space, Typography, Image, Button } from "antd";
+import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import img from "../assets/404.svg";
+
+const { Title, Text } = Typography;
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Space direction="vertical" align="center" size="large">
+      <Image src={img} width={200} preview={false} />
+      <Title
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <FormattedMessage id="route.notFound" />
+      </Title>
+      <Text type="secondary">
+        <FormattedMessage id="route.notFound.message" />
+      </Text>
+      <Button type="primary">
+        <Link to="/">
+          <FormattedMessage id="route.main" />
+        </Link>
+      </Button>
+    </Space>
+  );
 };
 
 export default NotFound;
