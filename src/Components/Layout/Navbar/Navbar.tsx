026cb4cd73b660cad3325bf2aa037daf60400c9a
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-03-28 21:24:06 +0200

    feat: add burger menu

--- src/Components/Layout/Navbar/Navbar.tsx
+++ src/Components/Layout/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
-import { Menu } from "antd";
+import { useState } from "react";
+import { Menu, Drawer, MenuTheme, Space } from "antd";
 import { Header } from "antd/es/layout/layout";
 import { Link } from "react-router-dom";
 import LanguageDropdown from "../../../features/internationalization/LanguageDropdown";
 import styles from "./Navbar.module.scss";
 import { FormattedMessage } from "react-intl";
-
+import clsx from "clsx";
+import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
+import useIsMobile from "../../../hooks/useIsMobile";
 const navigationLinks = [
   {
     path: "/main",
@@ -17,32 +20,79 @@ const navigationLinks = [
 ];
 
 const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const isSmallScreen = useIsMobile();
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <Header className={styles.header}>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        className={styles.menu}
-        selectable={false}
-      >
-        {navigationLinks.map((item) => (
-          // using <MenuItem/> instead of <Menu.Item/> to avoid reloading the menu-header on changing the route
-          <MenuItem key={item.path} {...item} />
-        ))}
-      </Menu>
-      <LanguageDropdown />
+      {isSmallScreen ? (
+        <>
+          <MenuOutlined
+            className={styles.icon}
+            style={{ color: "white" }}
+            onClick={toggleDrawer}
+          />
+          <Drawer
+            placement="right"
+            open={isDrawerOpen}
+            onClose={() => setIsDrawerOpen(false)}
+            closable={false}
+            className={styles.drawer}
+          >
+            <CloseOutlined
+              className={styles.icon}
+              style={{ color: "black" }}
+              onClick={() => setIsDrawerOpen(false)}
+            />
+            <Navigation theme="light" onClick={() => setIsDrawerOpen(false)} />
+          </Drawer>
+        </>
+      ) : (
+        <Navigation theme="dark" onClick={() => setIsDrawerOpen(false)} />
+      )}
     </Header>
   );
 };
 
+const Navigation = ({
+  theme,
+  onClick,
+}: {
+  theme: MenuTheme;
+  onClick: () => void;
+}) => (
+  <>
+    <Menu
+      theme={theme}
+      mode={theme === "dark" ? "horizontal" : "vertical"}
+      className={clsx(styles.menu, {
+        [styles.menuDark]: theme === "dark",
+      })}
+      selectable={false}
+    >
+      {navigationLinks.map((item) => (
+        // using <MenuItem/> instead of <Menu.Item/> to avoid reloading the menu-header on changing the route
+        <MenuItem {...item} onClick={onClick} />
+      ))}
+    </Menu>
+    <LanguageDropdown />
+  </>
+);
+
 const MenuItem = ({
   path,
   label,
+  onClick,
 }: {
   path: string;
   label: string | React.ReactNode;
+  onClick: () => void;
 }) => (
-  <li>
+  <li onClick={onClick}>
     <Link to={path} className={styles.link}>
       {label}
     </Link>
---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
